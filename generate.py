22ccd045a862948616cac02f38b3cc0149db66c0
chatskybag <chatskybag@mail.ru>
2026-06-15 20:52:28 +0300

    upd

--- generate.py
+++ generate.py
@@ -5,6 +5,7 @@ background-color: #f4f1ef;
 import os
 from jinja2 import Template
 
+
 # Список ваших изображений в нужном порядке
 images = ["1.png", "2_new.png", "3.png", "4.png", "5.png"]
 fill_link = "https://docs.google.com/forms/d/e/1FAIpQLSeYOH_tiWbkGQbo0YM89ucBdW7G_cxbRpeNNZ4OgSdbNWVVgA/viewform?usp=dialog"
